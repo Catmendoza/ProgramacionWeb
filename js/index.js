c287bba4f07fefd5f5df2f74d7617ee293570707
Catalina Mendoza <mendo657@gmail.com>
2020-10-06 16:59:04 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, {});
+  var instances2 = M.Modal.init(document.querySelectorAll(".modal"), {});
+});
+
+var instance = M.Carousel.init({
+  fullWidth: true,
+});
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
